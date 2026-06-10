a1e480ca018e0f01b02bcd03fe4de913534e0e59
mm57878 <mm57878@zut.edu.pl>
2026-06-10 18:33:53 +0200

    lab-j skonczony

--- lab-jj/main.py
+++
@@ -1,7 +0,0 @@
-import sys
-import platform
-name = "Michał"
-user_id = "57878"
-python_version = platform.python_version()
-python_path = sys.executable
-print(f"Hello \033[33m{name}\033[0m (\033[33m{user_id}\033[0m). This environment is using Python version \033[33m{python_version}\033[0m at location \033[33m{python_path}\033[0m.")
